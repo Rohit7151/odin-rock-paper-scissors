15f2bab0a92ece04d42b0803b186b166ba979815
Rohit7151 <rohitkhaprecoding@gmail.com>
2024-09-06 20:17:15 +0530

    created logic for computer choice

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('hello world')
+console.log('hello world');
+function getRandomInt(max=3){
+    return Math.floor(Math.random()*max);
+}
+function getComputerChoice(){
+    let n = getRandomInt()
+    if (n == 0){
+        return 'rock';
+    }
+    else if (n == 1){
+        return 'paper'
+    }
+    else{
+        return 'scissors'
+    }
+
+}
+
+console.log(getComputerChoice())
